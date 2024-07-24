fd9504b86475566b3a8d85b6ecba8f0c8cc85af7
Hoang <114314239+Gink-Hoang@users.noreply.github.com>
2024-07-24 22:39:25 +0700

    Initial Commit

---
+++ TopTouristAttractions/TopTouristAttractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTouristAttractionsApp.swift
+//  TopTouristAttractions
+//
+//  Created by Đặng Hoàng on 24/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TopTouristAttractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
